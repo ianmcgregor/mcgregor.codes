ecaaab1968df97cfd99fbb68a779d5eb8cd704f2
Ian McGregor <ian@stinkdigital.com>
2015-08-12 18:58:16 +0100

    adds analytics and doc title update

--- src/index.js
+++ src/index.js
@@ -7,6 +7,7 @@ import App from './components/App';
 import Services from './components/Services';
 import Contact from './components/Contact';
 import Work from './components/Work';
+import track from './utils/track';
 
 const container = document.querySelector('[data-region="container"]');
 
@@ -23,8 +24,24 @@ const routes = (
     </Route>
 );
 
+function getDocTitle(pathname) {
+    const title = 'CGREGOR CODES';
+    return title + pathname
+        .replace('_', ' ')
+        .replace('/', ' / ')
+        .toUpperCase();
+}
+
+// init Google Analytics
+track.init('UA-66215708-1');
+
+// Router.run(routes, Router.HistoryLocation, (Handler, state) => {
 Router.run(routes, (Handler, state) => {
+    console.debug('--> Router.run pathname:', state.pathname, 'params:', JSON.stringify(state.params, null, 2));
+
     React.initializeTouchEvents(true);
-    // Router.run(routes, Router.HistoryLocation, (Handler, state) => {
     React.render(<Handler {...state}/>, container);
+
+    document.title = getDocTitle(state.pathname);
+    track.page(state.pathname);
 });
---
+++ src/utils/track.js
@@ -0,0 +1,46 @@
+'use strict';
+
+export default {
+    init: function(gaAccount) {
+        console.log('Initialize Google Analytics with account Id:', gaAccount);
+
+        /*eslint-disable*/
+        (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+		(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+		m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+		})(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+        /*eslint-enable*/
+
+        ga('create', gaAccount, 'auto');
+        ga('send', 'pageview');
+    },
+    page: function(page, title) {
+        console.log('track.page:', page);
+        if (!window.ga) {
+            console.warn('track.page: Google Analytics not initialized');
+            return;
+        }
+        window.ga('send', {
+            hitType: 'pageview',
+            page: page,
+            title: title || document.title
+        });
+    },
+    event: function(category, action, label, value) {
+        console.log('track.event:', category, action, label, value);
+        if (!window.ga) {
+            console.warn('track.event: Google Analytics not initialized');
+            return;
+        }
+        const config = {
+            hitType: 'event',
+            eventCategory: category,
+            eventAction: action
+        };
+        if (label) {
+            config.eventLabel = label;
+            config.eventValue = value;
+        }
+        window.ga('send', config);
+    }
+};
