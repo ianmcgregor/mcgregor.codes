4fe6ee0808ed2713b300c3a47d6b25aa406d36a5
Ian McGregor <ian@mcgregor.codes>
2015-12-19 02:01:52 -0800

    Improves project descriptions

--- src/components/Info.js
+++ src/components/Info.js
@@ -22,32 +22,29 @@ class Info extends React.Component {
 
         return (
             <section className="Info">
-                <div className="Info-inner">
-                    <header className="Info-header" ref="info">
-                            <h3 className="Info-title">
-                                {title}
-                            </h3>
-                            <button
-                                className="Info-year"
-                                onClick={selectProject.bind(undefined, slug)}>
-                                {year}
-                            </button>
-                    </header>
-                    <ul className="Info-text">
-                        {text.map((item) => (
-                            <li key={item.key}>{item.value}</li>
-                        ))}
-                    </ul>
-                    <Filter
-                        filter={filter}
-                        tags={tags}
-                        modifier="Filter--info"
-                        showCount={false}/>
-
-                    <a className="Info-link" href={link} target="_blank">
-                        <span className="Icon Icon--link"></span>
-                    </a>
-                </div>
+                <header className="Info-header" ref="info">
+                        <h3 className="Info-title">
+                            {title}
+                        </h3>
+                        <button
+                            className="Info-year"
+                            onClick={selectProject.bind(undefined, slug)}>
+                            {year}
+                        </button>
+                </header>
+                <ul className="Info-text">
+                    {text.map((item) => (
+                        <li key={item.key}>{item.value}</li>
+                    ))}
+                </ul>
+                <Filter
+                    filter={filter}
+                    tags={tags}
+                    modifier="Filter--info"
+                    showCount={false}/>
+                <a className="Info-link" href={link} target="_blank">
+                    <span className="Icon Icon--link"></span>
+                </a>
             </section>
         );
     }
